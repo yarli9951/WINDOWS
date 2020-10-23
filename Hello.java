02634f4069a95cd57e2a432b129becaa86277a50
vamsi <scvamsi@gmail.com>
2020-10-23 08:07:27 -0700

    adding

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 class Hello {
 
-public static void main(Static[] args
-		){
+public static void main(Static[] args)
+{
 	System.out.println("Hello vamsi");
+	System.out.println("Hello Devops");
 
 }
 
